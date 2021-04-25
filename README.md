042b744b5bfecb12ff3aa4660e34471f4420f512
Eric Lemmon <ec.lemmon@gmail.com>
2021-04-25 14:40:46 +0200

    added README.md place holder. created a cell assigner class that will make sure that each player gets different cells

---
+++ README.md
@@ -0,0 +1,6 @@
+Code by <a href=https://ericlemmon.net/>Eric Lemmon</a>, scores and music by <a href=https://www.annesophieandersen.com/>
+Anne Sophie Andersen</a> and Eric Lemmon
+
+This repository holds the scores, instructions, and code for the collaboration between Anne Sophie Andersen 
+and Eric Lemmon for their work titled PLACE_HOLDER_TITLE, which will be premiered on the 2021 New Music for String festival
+held in Aarhus and Copenhagen Denmark.
---
+++ cell_assigner.py
@@ -0,0 +1,31 @@
+import random
+import image_data_loader
+
+
+class CellAssigner:
+    def __init__(self, cells):
+        assert type(list()) == type(cells)
+        self.cells = cells
+
+    def __sub__(self, other):
+        cells = self.cells.copy()
+        for cell in other.cells:
+            if cell in self.cells:
+                cells.remove(cell)
+        return CellAssigner(cells)
+
+    def select_half_of_cells_randomly(self):
+        for _ in range(len(self.cells) // 2):
+            self.cells.remove(random.choice(self.cells))
+
+    def __str__(self):
+        return str(self.cells)
+
+
+if __name__ == "__main__":
+    ca = CellAssigner(image_data_loader.get_image_paths())
+    print(ca)
+    ca.select_half_of_cells_randomly()
+    print(ca)
+    ca2 = CellAssigner(image_data_loader.get_image_paths())
+    print(ca-ca2)
