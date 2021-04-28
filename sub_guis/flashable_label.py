27d593836af4a9ed537fd302b83b4242cfdbb978
Eric Lemmon <ec.lemmon@gmail.com>
2021-04-28 21:37:09 +0200

    Documentation for flashable_label.py

--- sub_guis/flashable_label.py
+++ sub_guis/flashable_label.py
@@ -16,7 +16,17 @@ import tkinter as tk
 
 
 class FlashableLabel(tk.Label):
+    """
+    A small flashable label class based on Bryan Oakley's answer on StackExchange.
+    """
     def flash(self, flashes=10, count=0):
+        """
+        Flashes the label.
+        :param flashes: the number of color changes, so the number of flashes will likely be experienced as
+        flashes / 2
+        :param count: count variable to recursively call the function while modifying the count elegantly.
+        :return: None
+        """
         bg = self.cget('background')
         fg = self.cget('foreground')
         self.configure(background=fg, foreground=bg)
