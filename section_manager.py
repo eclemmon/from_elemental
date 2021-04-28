a15d6997561dc028ee71e6f94cdeaea2ead8b16c
Eric Lemmon <ec.lemmon@gmail.com>
2021-04-28 22:22:11 +0200

    Documentation for section_manager.py

--- section_manager.py
+++ section_manager.py
@@ -11,35 +11,69 @@ __email__ = "ec.lemmon@gmail.com"
 __status__ = "Testing"
 
 class SectionManager:
+    """
+    A class that allows for the easy control and selection of sections.
+    """
     def __init__(self, sections):
+        """
+        Initializes the SectionManager object. Constructs a dictionary of sections, enumerating them by the range
+        of the section parameter's length.
+        :param sections: Sections as list of tuples, organized as (Section name [string], Section length [int]).
+        """
         self.sections = dict(zip(range(1, len(sections)+1), sections))
         self.current_section = 1
 
     def next(self):
+        """
+        Increments current section by one.
+        :return: None
+        """
         self.current_section += 1
 
     def get_current_section_name(self):
+        """
+        Gets the name of the current section.
+        :return: String of current section name.
+        """
         return self.sections[self.current_section][0]
 
     def get_current_section_timing(self):
+        """
+        Gets the duration of the current section.
+        :return: Integer of the current section length as seconds.
+        """
         return self.sections[self.current_section][1]
 
     def set_current_section(self, key):
+        """
+        Sets the SectionManager's current section.
+        :param key: Key to set the current section by.
+        :return: None
+        """
         if key < len(self.sections):
             self.current_section = key
         else:
             print("key must be less than {}, and an integer".format(len(self.sections)))
 
     def get_total_timing(self):
+        """
+        Gets the total timing of the sections dictionary.
+        :return: Integer of total seconds.
+        """
         total_seconds = 0
         for val in self.sections.values():
             total_seconds += val[1]
         return total_seconds
 
     def start_from_section(self, section_value):
+        """
+        A method that connects with the Timer class by setting the current section and also returning the start
+        time of that second.
+        :param section_value: Integer of desired section dict key.
+        :return: integer that represents the start time of the current section.
+        """
         timing = 0
         for i in range(1, section_value):
-
             timing += self.sections[i][1]
         self.current_section = section_value
         return timing
